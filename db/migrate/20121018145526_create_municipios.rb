1aef1f874c4c00d0f93e240e774777158b4324a0
fcarlos <fc2g@yahoo.com.mx>
2012-10-29 16:33:54 -0600

    Bd modificada

--- db/migrate/20121018145526_create_municipios.rb
+++ db/migrate/20121018145526_create_municipios.rb
@@ -1,8 +1,8 @@
 class CreateMunicipios < ActiveRecord::Migration
   def change
     create_table :municipios do |t|
-      t.string :nommun, limit => 35, :null => false;
-      t.string :depreg, limit =>2, :null => false;
+      t.string :nommun, :limit => 35, :null => false;
+      t.string :depreg, :limit =>2, :null => false;
       
       t.timestamps
     end
